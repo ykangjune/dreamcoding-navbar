65230723e8f71b814dce96f382c8e009a3dfca0a
ykangjune <83484640+ykangjune@users.noreply.github.com>
2021-05-10 10:26:34 +0900

    navbar -clone

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar .toggleBtn');
+const menu = document.querySelector('.navbar .menu');
+const icons = document.querySelector('.navbar .icons');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
